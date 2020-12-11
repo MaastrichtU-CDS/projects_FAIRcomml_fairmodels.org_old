b2d916c544077ebfa40636603f48c5e38e5f7b83
Johan van Soest <info@johanvansoest.nl>
2020-12-11 11:47:13 +0100

    first try with rdflib and rdf service

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RdfService } from './rdf.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'webapp';
+
+  constructor(private rdfService: RdfService) {}
 }
--- webapp/src/app/rdf.service.ts
+++ webapp/src/app/rdf.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+// const rdflib = require('rdflib');
+import { IndexedFormula, Fetcher} from 'rdflib';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RdfService {
+  private store;
+  private indexUrl = "https://gitlab.com/UM-CDS/projects/FAIRcomml/fairmodels.org/-/raw/master/index.ttl";
 
-  constructor() { }
+  constructor() {
+    console.log("Init of RdfService!")
+
+    this.store = new IndexedFormula();
+    this.importTriples(this.indexUrl);
+  }
+
+  private importTriples(urlToLoad: string) {
+    var fetcher = new Fetcher(this.store);
+    fetcher.nowOrWhenFetched(urlToLoad, function(ok, body, xhr) {
+      if (!ok) {
+        console.log("Oops, something happened and couldn't fetch data");
+      } else {
+        console.log("Loaded statements.");
+      }
+    });
+    console.log("Number of statements loaded: " + this.store.length);
+  }
 }
